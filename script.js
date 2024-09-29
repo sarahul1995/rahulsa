ee19b2715705dbd201c149060c2679e74a03afcf
Rahul <123059504+sarahul1995@users.noreply.github.com>
2024-09-29 01:23:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.querySelectorAll('nav ul li a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
         const targetId = this.getAttribute('href');
+        print(targetId);
         const targetElement = document.getElementById(targetId);
       targetElement.scrollIntoView({ behavior: 'smooth' });
     });

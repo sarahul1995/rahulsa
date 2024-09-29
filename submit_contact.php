d9f9870ade183df56d7ae10e9bd7e5e2e93ef67f
Rahul <123059504+sarahul1995@users.noreply.github.com>
2024-09-29 02:41:47 -0700

    Update submit_contact.php

--- submit_contact.php
+++ submit_contact.php
@@ -1,11 +1,12 @@
 <!-- submit_contact.php -->
 <?php
 // Database connection parameters
-$serverName = "DESKTOP-348H65V"; // e.g., localhost
+$serverName = "DESKTOP-348H65V\SQLEXPRESS"; // e.g., localhost
 $connectionOptions = array(
     "Database" => "website", // your database name
     "Uid" => "DESKTOP-348H65V\Rahul Das", // your SQL Server username
-    "PWD" => "sa" // your SQL Server password
+    "PWD" => "" // your SQL Server password
+    "TrustServerCertificate" => true
 );
 
 // Establishes the connection
@@ -21,11 +22,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Get form data
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
     $message = htmlspecialchars($_POST['message']);
 
     // Prepare and execute the SQL statement
-    $sql = "INSERT INTO contacts (name, email, message) VALUES (?, ?, ?)";
-    $params = array($name, $email, $message);
+    $sql = "INSERT INTO contacts (name, email, subject, message) VALUES (?, ?, ?, ?)";
+    $params = array($name, $email, $subject, $message);
 
     $stmt = sqlsrv_query($conn, $sql, $params);
 
